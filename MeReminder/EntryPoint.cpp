e26ff4c2051218ad023bc13844b224c74933f638
roflixd <roflixd25@gmail.com>
2024-02-29 23:42:10 +0300

    test cmd command from cpp

--- MeReminder/EntryPoint.cpp
+++ MeReminder/EntryPoint.cpp
@@ -2,22 +2,6 @@
 
 int main(int argc, char* argv[])
 {
-   std::cout << "hello";  // dfdfd
-   return 0;
-}
-
-int function()
-{
-   for (size_t i = 0; i < 5; i++)
-   {
-      i++;
-   }
-   int g = 1;
-   switch (g)
-   {
-      case 0:
-         break;
-      default:
-         break;
-   }
+    system("git version");
+    return 0;
 }
