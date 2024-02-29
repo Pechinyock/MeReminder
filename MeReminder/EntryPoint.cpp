cd453ebabf7ceba237eb1b5b8ba7ea6935eed0a0
roflixd <roflixd25@gmail.com>
2024-03-01 00:42:07 +0300

    TODO

--- MeReminder/EntryPoint.cpp
+++ MeReminder/EntryPoint.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-
+/*
+ * TODO:
+ * minimal:
+ *   write to index.html file
+ *   args from cmd is text to write
+ */
 int main(int argc, char* argv[])
 {
     system("git version");
